92e9447c49fb19bb86c7cd3d9ff8dbed6f993df6
sohyeon <sohyun9743@naver.com>
2025-02-20 00:06:53 -0800

    font styles 추가

--- src/App.tsx
+++ src/App.tsx
@@ -6,29 +6,29 @@ import { UIText, UIContent } from './components/v1';
 function App() {
   return (
    <UIContent bgColor='eee'>
-     <div className="">
-        <p className='test'>테스트입니다.</p>
-
-
-<UIText variant="h1" as="h1" size='xl'>
-        Heading Text
-      </UIText>
-
-      {/* h2 텍스트 */}
-      <UIText variant="h2" as="h2" size='lg'>
-        Subheading Text
-      </UIText>
-
-      {/* 본문 텍스트 */}
-      <UIText variant="body" as="p" size='sm'>
-        This is a body text.
-      </UIText>
-
-      {/* 캡션 텍스트 */}
-      <UIText variant="caption">Caption Text</UIText>
-      {/* <UITextHeader.Basic>test</UITextHeader.Basic> */}
     
-    </div>
+    <p style={{fontFamily: "Bagel fat one"}}>S.H.LEE</p>
+
+
+    <UIText variant="h1" as="h1" size='xl'>
+      Heading Text
+    </UIText>     
+
+    {/* h2 텍스트 */}
+    <UIText variant="h2" as="h2" size='lg'>
+      Subheading Text
+    </UIText>
+
+    {/* 본문 텍스트 */}
+    <UIText variant="body" as="p" size='sm'>
+      This is a body text.
+    </UIText>
+
+    {/* 캡션 텍스트 */}
+    <UIText variant="caption">Caption Text</UIText>
+    {/* <UITextHeader.Basic>test</UITextHeader.Basic> */}
+    
+
    </UIContent>
 
     
