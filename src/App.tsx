055d4321a20fa7d748dd7f74f9a4651a1c6c0014
sohyeon <sohyun9743@naver.com>
2025-02-21 23:46:40 -0800

    App.tsx update

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,49 @@
 import React from 'react';
 import "./styles/style.scss";
-import { UIText, UIContent } from './components/v1';
+import { UIText, UIContent, UIHeader, UIBox } from './components/v1';
 
 
 function App() {
   return (
-  <UIContent bgColor='gray'>
-    
-    <p style={{fontFamily: "Bagel fat one"}}>S.H.LEE</p>
+<>
+  <UIContent bgColor='gray' bgPattern='check'>
+    <UIBox>
+      <UIHeader size='md'>
+        <p style={{fontFamily: "Bagel fat one", fontSize: "30px"}}>S.H.LEE</p>
+      </UIHeader>
+    </UIBox>
+    <UIBox align='center' style={{paddingTop: "200px"}}>
+      <div style={{paddingTop: "102px"}}>
+        <UIText variant="mainTitle" as="h1" size='xl'>
+          UI &middot; UX
+          <br/>
+          WEB PUBLISHER
+        </UIText>     
 
+        <UIText variant="mainTitle" as="h1" size='xl'>
+          PORTFOLIO
+        </UIText>
 
-    <UIText variant="h1" as="h1" size='xl'>
-      Heading Text
-    </UIText>     
+        {/* h2 텍스트 */}
+        {/* <UIText variant="h2" as="h2" size='lg'>
+          Subheading Text
+        </UIText> */}
 
-    {/* h2 텍스트 */}
-    <UIText variant="h2" as="h2" size='lg'>
-      Subheading Text
-    </UIText>
+        {/* 본문 텍스트 */}
+        {/* <UIText variant="p" as="p" size='sm'>
+          This is a body text.
+        </UIText> */}
 
-    {/* 본문 텍스트 */}
-    <UIText variant="body" as="p" size='sm'>
-      This is a body text.
-    </UIText>
-
-    {/* 캡션 텍스트 */}
-    <UIText variant="caption">Caption Text</UIText>
-    {/* <UITextHeader.Basic>test</UITextHeader.Basic> */}
-    
-
+        {/* 캡션 텍스트 */}
+        {/* <UIText variant="caption">Caption Text</UIText> */}
+      </div>
+    </UIBox>
   </UIContent>
+
+  <UIContent bgColor='black'>
+    <UIText color='#fff'>hee</UIText>
+  </UIContent>
+  </>
 
     
   );
--- src/components/v1.ts
+++ src/components/v1.ts
@@ -1,4 +1,6 @@
 export * from "./UIText/index";
 export * from "./UIContent/index";
+export * from "./UIHeader/index";
+export * from "./UIBox/index";
 // export * from "./UISwiper/index";
-// export * from "./UITextHeader/index";
+
