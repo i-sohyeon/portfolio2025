3a88e15d5aea528590c05456565b299124d10770
sohyeon <sohyun9743@naver.com>
2025-11-02 11:27:22 +0900

    서브페이지 리로딩시 404에러 오류 해결

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
-// src/App.jsx
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import { Route, HashRouter as Router, Routes } from 'react-router-dom';
-// GitHub Pages가 서버 리다이렉트 설정을 지원하지 않아 HashRouter로 수정
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from "./Routes/Home";
 import Sub from "./Routes/Sub";
 import { UIScroll } from "./components/UIScroll";
 
-// const basename = process.env.NODE_ENV === 'production' ? "/portfolio2025" : "";
-
 function App() {
+  const isProd = process.env.NODE_ENV === 'production';
   return (
-    <Router basename="/">
-      {/* 배포할때는 basename="/portfolio2025" 추가, 로컬에서는 삭제 */}
+    <Router basename={isProd ? '/portfolio2025' : '/'}>
       <UIScroll.ToTop />
       <Routes>
         <Route path="/" element={<Home />} />
@@ -22,5 +15,4 @@ function App() {
     </Router>
   );
 }
-
 export default App;
